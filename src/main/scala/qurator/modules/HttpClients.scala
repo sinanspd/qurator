4e6752e78d6329a4a8931d97f85756cde86e5a38
Sinan Pehlivanoglu <sinanspd@gmail.com>
2026-02-02 01:19:47 -0500

    bundle the clients for the scheduler

---
+++ src/main/scala/qurator/modules/HttpClients.scala
@@ -0,0 +1,31 @@
+package qurator.modules
+
+
+import cats.effect.MonadCancelThrow
+import org.http4s.circe.JsonDecoder
+import org.http4s.client.Client
+import dev.profunktor.redis4cats.RedisCommands
+import org.typelevel.log4cats.Logger
+import cats.effect.Async
+import qurator.clients._
+import qurator.Types._
+import fs2.hashing.Hashing
+
+object HttpClients {
+  def make[F[_]: JsonDecoder: MonadCancelThrow: Logger: Async : Hashing](
+      cfg: AppConfig,
+      client: Client[F],
+      redis: RedisCommands[F, String, String]
+  ): HttpClients[F] =
+    new HttpClients[F] {
+        def ibm: IBMClient[F] = IBMClient.make[F](cfg.ibmCredentials, client)
+        def braket: BraketClient[F] = BraketClient.make[F](cfg.braketConfig, client)
+        def azure: AzureQuantumClient[F] = AzureQuantumClient.make[F](cfg.azureConfig, client)  
+    }
+}
+
+sealed trait HttpClients[F[_]] {
+  def ibm: IBMClient[F]
+  def braket: BraketClient[F]
+  def azure: AzureQuantumClient[F]
+}
